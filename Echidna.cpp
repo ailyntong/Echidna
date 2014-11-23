c5c5fca1aef8a966ebcb5d211832cc6df00d2a08
ailyn tong <athappiness@gmail.com>
2014-11-23 15:52:46 -0800

    added blank methods

--- Echidna.cpp
+++ Echidna.cpp
@@ -4,10 +4,17 @@
 class Echidna : public IterativeRobot {
 public:
 	Echidna();
-	void robotInit();
-	void teleopInit();
-	void teleopPeriodic();
-	void teleopDisabled();
+	void RobotInit();
+	void AutonomousInit();
+	void AutonomousPeriodic();
+	void AutonomousDisabled();
+	void DisabledInit();
+	void DisabledPeriodic();
+	void TeleopInit();
+	void TeleopPeriodic();
+	void TeleopDisabled();
+	void TestInit();
+	void TestPeriodic();
 private:
 	Robot robot;
 	HumanController human;
@@ -20,23 +27,51 @@ human()
 	
 }
 
-void Echidna::robotInit() {
+void Echidna::RobotInit() {
 	robot.init();
 }
 
-void Echidna::teleopInit() {
+void Echidna::AutonomousInit(){
+	
+}
+
+void Echidna::AutonomousPeriodic() {
+
+}
+
+void Echidna::AutonomousDisabled() {
+	
+}
+
+void Echidna::DisabledInit() {
+
+}
+
+void Echidna::DisabledPeriodic() {
+
+}
+
+void Echidna::TeleopInit() {
 	robot.init();
 	robot.update();
 }
 
-void Echidna::teleopPeriodic() {
+void Echidna::TeleopPeriodic() {
 	human.update(&robot);
 	robot.update();
 }
 
-void Echidna::teleopDisabled() {
+void Echidna::TeleopDisabled() {
 	robot.disable();
 	robot.update();
+}
+
+void Echidna::TestInit() {
+
+}
+
+void Echidna::TestPeriodic() {
+
 }
 
 START_ROBOT_CLASS(Echidna);
